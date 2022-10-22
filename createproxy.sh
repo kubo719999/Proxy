929cabfbec03ebe6c261381fb957f7cd33f383ac
kubo719999 <96237808+kubo719999@users.noreply.github.com>
2022-10-22 18:23:37 +0700

    Update createproxy.sh

--- createproxy.sh
+++ createproxy.sh
@@ -41,8 +41,12 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth none
-$(awk -F "/" '{print "auth none\n" \
+auth strong
+
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
@@ -54,21 +58,10 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-upload_proxy() {
-    cd $WORKDIR
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -F "file=@proxy.zip" https://file.io)
 
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
-
-}
-    
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "thanhit/thanhit/$IP4/$port/$(gen64 $IP6)/$NETWORK_INTERFACE_NAME"
+        echo "user$port/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
